1560265bfa7c4b033af93a9871da0b9e40093c39
Prince <princenocode@gmail.com>
2026-05-09 19:35:08 +0200

    chore: drop commitlint deps and config

--- commitlint.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('@commitlint/types').UserConfig} */
-export default {
-	extends: ["@commitlint/config-conventional"],
-	rules: {
-		"header-max-length": [2, "always", 100],
-		"body-max-line-length": [2, "always", 200],
-	},
-};
